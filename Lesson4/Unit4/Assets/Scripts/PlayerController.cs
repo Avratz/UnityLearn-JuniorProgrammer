74af3089cd7ab9315240a11e41fc3a8c1880b6b3
Avratz <signos97@gmail.com>
2022-08-11 09:30:51 -0300

    Player Movement and enemy follow-player behaviour

--- Lesson4/Unit4/Assets/Scripts/PlayerController.cs
+++ Lesson4/Unit4/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float speed = 5.0f;
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = focalPoint.transform.forward;
+        playerRb.AddForce(direction * (verticalInput * speed));
     }
 }
---
+++ Lesson4/Unit4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed = 50;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
