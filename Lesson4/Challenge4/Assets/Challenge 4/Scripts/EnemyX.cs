5432713760bb4c2c3f0a20c76b47a4ad1f4655b1
Avratz <signos97@gmail.com>
2022-08-15 13:11:21 -0300

    fixes and two bonus

---
+++ Lesson4/Challenge4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    [SerializeField] private float speed = 300f;
+    private float speedIncreaseModifier = 10;
+    private float maximumSpeed = 500f; // same as player without boost
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        
+        enemyRb.AddForce(lookDirection * (speed * Time.deltaTime));
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void IncrementSpeed(int waveCount)
+    {
+        if(speed < maximumSpeed){
+            speed = speed + waveCount * speedIncreaseModifier;
+        }
+    }
+}
